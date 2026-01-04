43919ac96df564b1b7a4c119b9d931c6c062979e
Brent Peterson <creativedata@gmail.com>
2026-01-04 05:59:49 -1000

    Initial Commit

---
+++ BrentTimeKeeper/BrentTimeKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrentTimeKeeperApp.swift
+//  BrentTimeKeeper
+//
+//  Created by Brent Peterson on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BrentTimeKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
